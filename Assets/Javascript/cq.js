1a783753da4903356c668f68a7e254a1f424ee0b
SCN <supercodingninja@outlook.com>
2020-12-22 13:09:18 -0800

    Syntax "," is everything!  I just fixed 69 bugs I discovered in my editor.  I also decideed to make my "choiceSection" in my .html file have separate <div>.

--- Assets/Javascript/cq.js
+++ Assets/Javascript/cq.js
@@ -11,128 +11,128 @@ const instructEl = document.getElementById('instruct');
 
 // Q & A //
 const questions = [{
-    Q: "What instrument does `'Fred'` play?"
+    Q: "What instrument does `'Fred'` play?",
 
-    choice1: "Saxophone"
-    choice2: "`Fred` thinks all band members are geeks."
-    choice3: "`Fred` doesn't play `an` instrument."
+    choice1: "Saxophone",
+    choice2: "`Fred` thinks all band members are geeks.",
+    choice3: "`Fred` doesn't play `an` instrument.",
+
+    A: "3",
+},{
+    Q: "How many instruments can `Fred` play?",
+
+    choice1: "Most saxophones, baritone, flute.",
+    choice2: "Piano, guitar, drums.",
+    choice3: "Anything he puts his mind to.",
 
     A: 3,
 },{
-    Q: "How many instruments can `Fred` play?"
+    Q: "How many times has `Fred` been married?",
 
-    choice1: "Most saxophones, baritone, flute."
-    choice2: "Piano, guitar, drums."
-    choice3: "Anything he puts his mind to."
-
-    A: 3,
-},{
-    Q: "How many times has `Fred` been married?"
-
-    choice1: "`Fred` got married: that's a joke!"
-    choice2: "`Fred` is a priest, and not allowed to get married."
-    choice3: "`Fred` lives with his wife in the United States, has a wife in Djibouti, Singapore; and Cairns, Australia (Sydney is debatable, and not yet, proven by Common Wealth law)."
-    choice4: "`Fred` has been married twice: his first wife was deported by higher command's orders, while serving in the Marines."
-    choice5: "`Fred` has only been married once, and plans to stay that way (one and done)!"
+    choice1: "`Fred` got married: that's a joke!",
+    choice2: "`Fred` is a priest, and not allowed to get married.",
+    choice3: "`Fred` lives with his wife in the United States, has a wife in Djibouti, Singapore; and Cairns, Australia (Sydney is debatable, and not yet, proven by Common Wealth law).",
+    choice4: "`Fred` has been married twice: his first wife was deported by higher command's orders, while serving in the Marines.",
+    choice5: "`Fred` has only been married once, and plans to stay that way (one and done)!",
 
     A: 5,
 },{
-    Q: "How does `Fred` prefer to be addressed?"
+    Q: "How does `Fred` prefer to be addressed?",
 
-    choice1: "Not late for dinner!"
-    choice2:"`Fred`: hello, the game is named after him!  Keep it simple, LOL!"
-    choice3: "Turbo!"
-    choice4: "`Fred` doesn't care, that's why he's a great guy!"
-    choice5: "Frederick"
+    choice1: "Not late for dinner!",
+    choice2:"`Fred`: hello, the game is named after him!  Keep it simple, LOL!",
+    choice3: "Turbo!",
+    choice4: "`Fred` doesn't care, that's why he's a great guy!",
+    choice5: "Frederick",
 
     A: 5,
 },{
-    Q: "What is the nickname for `Fred`?"
+    Q: "What is the nickname for `Fred`?",
 
-    choice1: "Really?!"
-    choice2: "derF"
-    choice3: "Freddy"
-
-    A: 1,
-},{
-    Q: "What is `Frederick's` favorite nickname?"
-
-    choice1: "`Fred`"
-    choice2: "Turbo"
-    choice3: "Motto"
-
-    A: 2,
-},{
-    Q: "What is `Frederick's` religion?"
-
-    choice1: "`Fred` was a devout Muslim, since he served in the wars.  He met his wife in Baharain, while detached to a U. S. Naval base; and was converted to Christianity, when his father-in-law gave him a choice, `Your religion, or my daughter.`"
-    choice2: "`Fred` believes in Christianity, Judaism, and Muslim (all from father Abraham)."
-    choice3: "`Frederick's` Faith is Christianity."
-
-    A: 3,
-},{
-    Q: "`Fred's` favorite show ___."
-
-    choice1: "is Arrow, Season 4"
-    choice2: "is Stargate: Alantis"
-    choice3: "is The Mandolorian"
-    choice4: "is DudeDad"
-    choice5: "is DryBar Comedy"
-    choice6: "is Corbra Kai"
-    choice7: "is Stargate SG-6"
-    choice8: "is Star Trek: Borg Wars"
-    choice9: "is Mighty Morphing Power Rangers"
-    choice10: "is Star Wars"
-
-    A: 3,
-},{
-    Q: "What is `Fred's` favorite show?"
-
-    choice1: "The Mandolorian"
-    choice2: "Star Wars: The Clone Wars"
-    choice3: "Game of Horns"
-
-    A: 2,
-},{
-    Q: "How old is `Fred`?"
-
-    choice1: "non3 0f your business."
-    choice2: "28"
-    choice3: "Who lik3s c8ke, anyway:"
-    choice4: "35"
-    choice5: "It's r38lly none of your busin3ss."
-    choice6: "37"
-    choice7: "4 the last time, non3 of your business!"
-
-    A: 3,
-},{
-    Q: "`Fred` likes to ___."
-
-    choice1: "Skydive"
-    choice2: "Surf"
-    choice3: "Climb Mountains"
-
-    A: 2,
-},{
-    Q: "`Fred` likes to ___."
-
-    choice1: "Climb Rockwalls"
-    choice2: "Fish"
-    choice3: "Run"
+    choice1: "Really?!",
+    choice2: "derF",
+    choice3: "Freddy",
 
     A: 1,
 },{
-    Q: "`Fred` was engaged three times."
+    Q: "What is `Frederick's` favorite nickname?",
 
-    choice1: "True"
-    choice2: "False"
+    choice1: "`Fred`",
+    choice2: "Turbo",
+    choice3: "Motto",
+
+    A: 2,
+},{
+    Q: "What is `Frederick's` religion?",
+
+    choice1: "`Fred` was a devout Muslim, since he served in the wars.  He met his wife in Baharain, while detached to a U. S. Naval base; and was converted to Christianity, when his father-in-law gave him a choice, `Your religion, or my daughter.`",
+    choice2: "`Fred` believes in Christianity, Judaism, and Muslim (all from father Abraham).",
+    choice3: "`Frederick's` Faith is Christianity.",
+
+    A: 3,
+},{
+    Q: "`Fred's` favorite show ___.",
+
+    choice1: "is Arrow, Season 4",
+    choice2: "is Stargate: Alantis",
+    choice3: "is The Mandolorian",
+    choice4: "is DudeDad",
+    choice5: "is DryBar Comedy",
+    choice6: "is Corbra Kai",
+    choice7: "is Stargate SG-6",
+    choice8: "is Star Trek: Borg Wars",
+    choice9: "is Mighty Morphing Power Rangers",
+    choice10: "is Star Wars",
+
+    A: 3,
+},{
+    Q: "What is `Fred's` favorite show?",
+
+    choice1: "The Mandolorian",
+    choice2: "Star Wars: The Clone Wars",
+    choice3: "Game of Horns",
+
+    A: 2,
+},{
+    Q: "How old is `Fred`?",
+
+    choice1: "non3 0f your business.",
+    choice2: "28",
+    choice3: "Who lik3s c8ke, anyway:",
+    choice4: "35",
+    choice5: "It's r38lly none of your busin3ss.",
+    choice6: "37",
+    choice7: "4 the last time, non3 of your business!",
+
+    A: 3,
+},{
+    Q: "`Fred` likes to ___.",
+
+    choice1: "Skydive",
+    choice2: "Surf",
+    choice3: "Climb Mountains",
+
+    A: 2,
+},{
+    Q: "`Fred` likes to ___.",
+
+    choice1: "Climb Rockwalls",
+    choice2: "Fish",
+    choice3: "Run",
 
     A: 1,
 },{
-    Q: "`Fred` likes to wear Captain America and Iron Man shirts, while working out."
+    Q: "`Fred` was engaged three times.",
 
-    choice1: "True"
-    choice2: "False"
+    choice1: "True",
+    choice2: "False",
+
+    A: 1,
+},{
+    Q: "`Fred` likes to wear Captain America and Iron Man shirts, while working out.",
+
+    choice1: "True",
+    choice2: "False",
 
     A: 2,
 },]
@@ -330,7 +330,7 @@ introBtnEl.addEventListener('click', beginQuiz);
 redoBtnEl.addEventListener('click', redoQuiz);
 topEl.addEventListener('click', redoQuiz);
 
-// This event listener will allow the user to store their score and add their initials.
+// This event listener will allow the user to store their score and add their initials. //
 totalEl.addEventListener('click', johnHancock);
 
 goToScoreboardEl.addEventListener('click', () => {
